3bf932259e048ceee0fbfa955fc255aabba397a2
Zan <yzcheema11@gmail.com>
2020-10-03 10:09:25 -0400

    Adding first method and test

--- src/main/java/com/dtcc/projects/PhoneBook.java
+++ src/main/java/com/dtcc/projects/PhoneBook.java
@@ -1,4 +1,32 @@
 package com.dtcc.projects;
 
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PhoneBook {
+
+    Map<String, List<String>> phoneBookMap;
+
+    public PhoneBook(){
+        this(new LinkedHashMap<String, List<String>>());
+    }
+
+    public PhoneBook(Map<String, List<String>> map) {
+        this.phoneBookMap = map;
+    }
+
+    public void add (String name, String phoneNumber) {
+        List<String> phoneNumberList = phoneBookMap.get(name);
+        if (phoneNumberList != null) {
+            phoneNumberList.add(phoneNumber);
+        }
+        else {
+            phoneBookMap.put(name, Collections.singletonList(phoneNumber));
+        }
+    }
+
+
 }
--- src/test/java/com/dtcc/projects/PhoneBookTest.java
+++ src/test/java/com/dtcc/projects/PhoneBookTest.java
@@ -1,4 +1,17 @@
 package com.dtcc.projects;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PhoneBookTest {
+
+    @Test
+    public void addTest(){
+        PhoneBook phoneBook = new PhoneBook();
+        String name = "Zan";
+        String phoneNumber = "123432152";
+        phoneBook.add(name, phoneNumber);
+        Assert.assertTrue(phoneBook.phoneBookMap.containsKey(name));
+        Assert.assertEquals(phoneNumber,phoneBook.phoneBookMap.get(name).get(0));
+    }
 }
